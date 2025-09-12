8d70370dd65c274ba7aafb2190c63eb8cbe3d040
alekss211 <alekss.bdr211@gmail.com>
2025-09-12 22:15:59 +0200

    Fix: Build Error

--- service-apprunner/src/main/java/org/mustangproject/serviceapprunner/ServiceAppRunnerApplication.java
+++ service-apprunner/src/main/java/org/mustangproject/serviceapprunner/ServiceAppRunnerApplication.java
@@ -1,4 +1,4 @@
-package main.java.org.mustangproject.serviceapprunner;
+package org.mustangproject.serviceapprunner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
