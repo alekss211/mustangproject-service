4260db430c122c914223cf31fb8279779308e0ac
alekss211 <alekss.bdr211@gmail.com>
2025-09-18 17:20:39 +0200

    #17: Implementing /test-interface REST endpoint for testing purposes

--- service-apprunner/src/main/java/org/mustangproject/serviceapprunner/InvoiceController.java
+++ service-apprunner/src/main/java/org/mustangproject/serviceapprunner/InvoiceController.java
@@ -1,8 +1,12 @@
 package org.mustangproject.serviceapprunner;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -16,5 +20,16 @@ public class InvoiceController {
     @GetMapping("/info")
     public String info() {
         return "Mustang Invoice Service - E-invoice processing and validation";
+    }
+
+    @GetMapping("/test-interface")
+    public ResponseEntity<Map<String, Object>> testInterface() {
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "success");
+        response.put("message", "Success - mustangproject-service is available");
+        response.put("service", "mustangproject-service");
+        response.put("version", "2.19.1-SNAPSHOT");
+        
+        return ResponseEntity.ok(response);
     }
 }
